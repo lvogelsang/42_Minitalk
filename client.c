a0f82b507bcc8dd294bd92b6be8a2a50af963a9e
Lena Vogelsang <lenav@Lenas-MBP.modem>
2023-01-19 15:16:17 +1030

    comments

--- client.c
+++ client.c
@@ -6,11 +6,22 @@
 /*   By: lvogelsa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 10:20:48 by lvogelsa          #+#    #+#             */
-/*   Updated: 2023/01/19 11:01:27 by lvogelsa         ###   ########.fr       */
+/*   Updated: 2023/01/19 15:13:18 by lvogelsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+// Once we know the PID of the server, the client program can be called. It
+// takes two arguments - the server's PID and the message that we want to send
+// to the server. An error will be displayed if there is an invalid number of
+// arguments.
+
+// If the program call is valid, we first transform the PID into an integer as
+// this format is needed for the later called kill() function.
+
+// Then, we initialize the process of sending the message to the server by
+// using the function send_bit().
 
 int	main(int argc, char **argv)
 {
@@ -21,13 +32,32 @@ int	main(int argc, char **argv)
 	else
 	{
 		pid = ft_atoi(argv[1]);
-		if (pid == 0)
-			ft_printf("Error! Bad PID!\n");
-		else
-			send_bit(pid, argv[2], ft_strlen(argv[2]));
+		send_bit(pid, argv[2], ft_strlen(argv[2]));
 	}
 	return (0);
 }
+
+// The send_bit() function takes three arguments - the server's PID, the
+// message, and the length of the message.
+
+// We loop through the message to access each of its characters to then
+// encode it and send a signal to the server.
+
+// Digital signals are binary signals, i.e., they are composed of only 0s and 1s.
+// The ASCII characters that we are trying to send to the server can be 
+// written in binary with 8 bits each. Therefore, we are looping through the
+// binary representation of each character 8 times. The "shift" variable
+// demonstrates the bit position, here going from right (pos 0) to left (pos 7).
+
+// The single ampersand is a bitwise AND operator. In our case, it basically
+// compares the bit at the specified position and 1. If they are the same, i.e.,
+// if the bit at the specified position is 1, we send the SIGUSR1 signal to the 
+// server. Otherwise, if the bit at the specified position is 0, we send the
+// SIGUSR2 to the server.
+
+// The usleep(x) function suspends execution of the program for x
+// microseconds. The purpose is to allow sufficient time for the signal to be
+// sent to and processed by the server.
 
 void	send_bit(int pid, char *message, int len)
 {
@@ -37,15 +67,15 @@ void	send_bit(int pid, char *message, int len)
 	i = 0;
 	while (i <= len)
 	{
-		shift = 7;
-		while (shift >= 0)
+		shift = 0;
+		while (shift < 7)
 		{
-			if ((message[i] >> shift) & 1)
+			if ((message[i] << shift) & 1)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
 			usleep(100);
-			shift--;
+			shift++;
 		}
 		i++;
 	}
--- server.c
+++ server.c
@@ -6,11 +6,22 @@
 /*   By: lvogelsa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 09:48:02 by lvogelsa          #+#    #+#             */
-/*   Updated: 2023/01/19 11:05:06 by lvogelsa         ###   ########.fr       */
+/*   Updated: 2023/01/19 15:15:38 by lvogelsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+// The server program is called first. It doesn't take any arguments - an
+// error will be displayed if there is an invalid number of arguments.
+
+// If the program call is valid, we extract the server's process identifier
+// (PID) with getpid(). A PID is a number to uniquely identify an active 
+// process (here the server program), which is needed to be able to send 
+// signals to the process and  manipulate it.
+
+// Signal()
+// Purpose of pause()
 
 int	main(int argc, char **argv)
 {
@@ -31,20 +42,35 @@ int	main(int argc, char **argv)
 	return (0);
 }
 
+// The bit_handler() takes a signal as an argument - in this case, it is 
+// either SIGUSR1 or SIGUSR2.
+
+// Similarly to client.c, the "shift" variable demonstrates the bit 
+// position - as we start from right (pos 0) to left (pos 7) in client.c, here,
+// we need to the same.
+
+// If the signal is SIGUSR1, we are placing a 1 into the specified position of
+// "byte".
+
+// Once the client has sent all 8 bits of the binary representation of the ASCII 
+// value and once they all have been handled, we can print the character ("byte")
+// and reset the bit_handler(), ready for the next character.
+
 void	bit_handler(int signal)
 {
 	static int	byte;
-	static int	location;
+	static int	shift;
 	
 	if (signal == SIGUSR1)
-		byte += 1 << (7 - location);
-	location++;
-	if (location == 8)
+		byte += 1 >> shift;
+	shift++;
+	// 7?
+	if (shift == 8)
 	{
 		ft_printf("%c", byte);
 		if (!byte)
 			ft_printf("\n");
 		byte = 0;
-		location = 0;
+		shift = 0;
 	}
 }
